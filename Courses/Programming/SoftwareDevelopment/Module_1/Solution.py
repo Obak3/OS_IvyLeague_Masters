3e807a8593a9449c89127804904aab19491bdada
Obak3 <Kchristopher1959@gmail.com>
2021-12-24 12:52:22 -0500

    Add the solution

---
+++ Courses/Programming/SoftwareDevelopment/Module_1/Solution.py
@@ -0,0 +1,46 @@
+'''
+This will act as your 'assignment' for the first module.
+
+Once you complete this, look at the solution in 'sol.py'!
+'''
+
+# Assign an integer of 7 to the variable 'x':
+x = 7
+
+# Assign an integer of 10 to the variable 'y':
+y = 10 
+
+
+# Add those two values together and set the sum to a variable 'z':
+z = x + y
+
+'''
+Check if 'z' is less than 20.
+    - If it is, then print out saying "The number is small"
+    - If it is not, then print out saying "The number is quite large"
+'''
+
+if z < 20:
+    print("The number is small")
+else:
+    print("The number is quite large")
+
+# Set a string to say "python is really cool" with the variable name 'first_string':
+first_string = "python is really cool"
+
+# Set a boolean value to true for a variable 'learning':
+learning = True
+
+'''
+Check if 'learning' is less than True:
+    - If it is, then print out saying "I'm learning something"
+    - If it is not, then print out saying "I'm not learning anything"
+'''
+
+# In the future... you can also just do ->>> if learning:
+if learning == True:
+    print("I'm learning something")
+else:
+    print("I'm not learning anything")
+
+# Kudos to anyone who **actually did this**
